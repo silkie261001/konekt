3e9d0f4c5187450d06063dd8e4be1406bac1bfe4
Silkie Agarwal <silkieagarwal261001@gmail.com>
2022-11-07 13:01:29 +0530

    msg

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,118 +1,110 @@
-import User from "../models/user"
-import { hashPassword,comparePassword } from "../helpers/auth.js";
+import User from '../models/user';
+import { hashPassword, comparePassword } from '../helpers/auth.js';
 import jwt from 'jsonwebtoken';
+import { nanoid } from 'nanoid';
 // import { expressjwt } from "express-jwt";
 
+export const register = async (req, res) => {
+  const { name, email, password, secret } = req.body;
 
-export const register = async(req,res) => {
-    const {name,email,password,secret} = req.body;
+  //validation
+  if (!name) return res.status(400).send('Name is required');
+  if (!password || password.length < 6)
+    return res
+      .status(400)
+      .send('password is required and should be 6 character long');
+  if (!secret) return res.status(400).send('answer is required');
+  const exist = await User.findOne({ email });
+  if (exist) return res.status(400).send('email is taken');
 
-    //validation 
-    if(!name) return res.status(400).send("Name is required");
-    if(!password || password.length < 6)
-        return res
-            .status(400)
-            .send("password is required and should be 6 character long");
-    if(!secret) return res.status(400).send("answer is required");
-    const exist = await User.findOne({email});
-    if(exist)
-        return res.status(400).send("email is taken");
-
-    // hash password
-    const hashedPassword = await hashPassword(password);
-    const user = new User({name, email, password:hashedPassword ,secret});
-    try{
-        // const hashpassword = await hashPassword(password);
-        // const user = new User({name, email, password:hashpassword ,secret});
-        await user.save();
-        // console.log("registered user =>" ,user); 
-        return res.json({
-            ok:true,
-        });
-    }catch(err){
-        console.log("register fail =>",err);
-        return res.status(400).send("error, try again");
-    }
-}
-
- 
-export const  login = async(req,res) =>{
-    // console.log(req.body)
-    try{
-        const {email,password} = req.body;
-        // check if our db has user with this database
-        const user = await User.findOne({email});
-        if(!user) return res.status(400).send("Invalid Credentials");
-        // check password
-        const match = await comparePassword(password,user.password);
-
-        if(!match ) return res.status(400).send("Invalid Credentials");
-
-        //create signed token
-        const token = jwt.sign({_id:user._id},process.env.JWT_SECRET, {expiresIn:"7d"})
-
-        user.password = undefined;
-        user.secret = undefined;
-        res.json({
-            token,
-            user,
-        });
-    } catch(err){
-        console.log(err)
-        return res.status(400).send("error try again");
-    }
+  // hash password
+  const hashedPassword = await hashPassword(password);
+  const user = new User({ name, email, password: hashedPassword, secret });
+  try {
+    // const hashpassword = await hashPassword(password);
+    // const user = new User({name, email, password:hashpassword ,secret});
+    await user.save();
+    // console.log("registered user =>" ,user);
+    return res.json({
+      ok: true,
+    });
+  } catch (err) {
+    console.log('register fail =>', err);
+    return res.status(400).send('error, try again');
+  }
 };
 
-export const currentUser = async(req,res) =>{
-    try{
-        const user = await User.findById(req.user._id);
-        res.json(user);
-        // console.log("doone");
-        res.json({
-            ok:true
-        })
-    }
-    catch(err){
-        console.log(err);
-        res.sendStatus(400);
-    }
+export const login = async (req, res) => {
+  // console.log(req.body)
+  try {
+    const { email, password } = req.body;
+    // check if our db has user with this database
+    const user = await User.findOne({ email });
+    if (!user) return res.status(400).send('Invalid Credentials');
+    // check password
+    const match = await comparePassword(password, user.password);
+
+    if (!match) return res.status(400).send('Invalid Credentials');
+
+    //create signed token
+    const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: '7d',
+    });
+
+    user.password = undefined;
+    user.secret = undefined;
+    res.json({
+      token,
+      user,
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(400).send('error try again');
+  }
 };
 
-export const forgotPassword = async (req,res) =>{
-    // console.log(req.body); 
-    try{
-        const { email,newPassword,secret} = req.body;
-        // validation 
-        console.log(email);
-        if(!newPassword || newPassword.length <6){
-            return res
-            .status(400)
-            .send("password is required and should be 6 character long");
-        }
+export const currentUser = async (req, res) => {
+  try {
+    const user = await User.findById(req.user._id);
+    res.json(user);
+    // console.log("doone");
+    res.json({
+      ok: true,
+    });
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(400);
+  }
+};
 
-        if(!secret){
-            return res
-            .status(400)
-            .send("Secret is required")
-        }
-
-        const user = await User.findOne({email, secret});
-        
-        if(!user){
-            return res
-            .status(400)
-            .send("Invalid Credentials");
-        }
-        const hashed = await hashPassword(newPassword);
-        await User.findByIdAndUpdate(user._id , {password: hashed});
-        return res
-            .status(400)
-            .send('password changed')
-    }catch(err){
-        console.log(err)
-        return res.json({
-            error:"something wrong",
-        })
+export const forgotPassword = async (req, res) => {
+  // console.log(req.body);
+  try {
+    const { email, newPassword, secret } = req.body;
+    // validation
+    console.log(email);
+    if (!newPassword || newPassword.length < 6) {
+      return res
+        .status(400)
+        .send('password is required and should be 6 character long');
     }
 
-}
+    if (!secret) {
+      return res.status(400).send('Secret is required');
+    }
+
+    const user = await User.findOne({ email, secret });
+
+    if (!user) {
+      return res.status(400).send('Invalid Credentials');
+    }
+    const hashed = await hashPassword(newPassword);
+    await User.findByIdAndUpdate(user._id, { password: hashed });
+    return res.status(400).send('password changed');
+  } catch (err) {
+    console.log(err);
+    return res.json({
+      error: 'something wrong',
+    });
+  }
+};
